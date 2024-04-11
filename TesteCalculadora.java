83cf4cad38d204b1fb2e3e8982bc60197b12c38d
João Vitor dos Santos <vitor40joao@gmail.com>
2024-04-10 23:30:59 -0300

    test(teste):testa funcao soma

---
+++ TesteCalculadora.java
@@ -0,0 +1,6 @@
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        var calculadora = new Calculadora();
+        System.out.println(calculadora.Soma(5, 3));
+    }
+}
